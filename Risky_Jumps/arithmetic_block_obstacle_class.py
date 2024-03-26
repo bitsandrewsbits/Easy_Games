51361309edd6fd0617ed295e2cc9e923dcd0f7dc
Andrew Kovalenko <kov.andreas.30@gmail.com>
2024-03-26 21:23:14 +0200

    Add method for class Arithm Block. Method defines road Y coordinates for putting block on road.

--- Risky_Jumps/arithmetic_block_obstacle_class.py
+++ Risky_Jumps/arithmetic_block_obstacle_class.py
@@ -6,8 +6,10 @@
 from random import randint
 
 class Arithmetic_Obstacle_Block:
-	def __init__(self, road_XY_coordinates = []):
+	def __init__(self, main_game_window_size = (300, 200), road_XY_coordinates = []):
+		self.main_game_window_width_height = main_game_window_size
 		self.roadline_XY_coordinates = road_XY_coordinates
+		self.road_Y_coordinates_for_block = []
 		self.left_math_operand = randint(10, 60)
 		self.math_operator = '+'
 		self.right_math_operand = randint(10, 60)
@@ -15,4 +17,9 @@ class Arithmetic_Obstacle_Block:
 		self.height_of_block = self.right_math_operand * 2
 		self.block_color_in_RGB = (10, 20, 90)
 		self.block_text_color_in_RGB = (200, 200, 200)
-		
+
+	def define_road_Y_coordinate_for_block_on_road(self):
+		for road_XY_pair in self.roadline_XY_coordinates:
+			if road_XY_pair[1] > self.main_game_window_width_height + 100:
+				self.road_Y_coordinates_for_block.append(road_XY_pair[1])
+
