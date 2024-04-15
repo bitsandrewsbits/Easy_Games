5880fdd94bcaa22da8ee9f16bb4ef1e86d9bb3da
Andrew Kovalenko <kov.andreas.30@gmail.com>
2024-04-15 21:38:27 +0300

    Modified random choosing XY pair for arithmetic block coordinates in Block class.

--- Risky_Jumps/arithmetic_block_obstacle_class.py
+++ Risky_Jumps/arithmetic_block_obstacle_class.py
@@ -13,7 +13,7 @@ class Arithmetic_Obstacle_Block:
 		self.main_game_window_width_height = main_game_window_size
 		self.roadline_XY_pairs_of_coordinates = road_XY_coordinates
 		self.road_XY_pairs_of_coordinates_for_block = []
-		self.randomly_choosed_XY_pair_for_block = self.get_randomly_choosed_XY_pair_of_roadline_for_block()
+		self.randomly_choosed_XY_pair_for_block = self.get_random_X_coordinate_and_just_Y_in_two_choosed_XY_pairs()
 		print('\nBlock coordinates:', self.randomly_choosed_XY_pair_for_block)
 		self.left_math_operand = randint(10, 60)
 		self.math_operator = '+'
@@ -31,9 +31,22 @@ class Arithmetic_Obstacle_Block:
 			if road_XY_pair[0] > self.main_game_window_width_height[0] + 130:
 				self.road_XY_pairs_of_coordinates_for_block.append(road_XY_pair)
 
-	def get_randomly_choosed_XY_pair_of_roadline_for_block(self):
+	def get_random_two_XY_pairs_of_line_as_part_of_road(self):
 		self.define_road_XY_coordinates_for_block_on_road()
-		return self.road_XY_pairs_of_coordinates_for_block[randint(0, len(self.road_XY_pairs_of_coordinates_for_block) - 1)]
+		first_random_XY_pair_index_in_coordinates = randint(0, len(self.road_XY_pairs_of_coordinates_for_block) - 2)
+		secound_XY_pair_index_in_coordinates = first_random_XY_pair_index_in_coordinates + 1
+		return [self.road_XY_pairs_of_coordinates_for_block[first_random_XY_pair_index_in_coordinates],
+				self.road_XY_pairs_of_coordinates_for_block[secound_XY_pair_index_in_coordinates]]
+
+	def get_random_X_coordinate_and_just_Y_in_two_choosed_XY_pairs(self):
+		two_random_XY_pairs = self.get_random_two_XY_pairs_of_line_as_part_of_road()
+		start_X_coordinate_value_for_choosing = two_random_XY_pairs[0][0]
+		end_X_coordinate_value_for_choosing = two_random_XY_pairs[1][0]
+		randomly_choosed_X_coordinate_for_block = randint(start_X_coordinate_value_for_choosing, end_X_coordinate_value_for_choosing)
+		Y_coordinate_for_block = two_random_XY_pairs[0][1]
+
+		return [randomly_choosed_X_coordinate_for_block, Y_coordinate_for_block]
+
 
 	def draw_block_on_game_road(self):
 		pg.draw.rect(self.pygame_window_obj, self.block_color_in_RGB, self.block_parameters_for_drawing)
